47b5be495606e4b79110f22272ff1f5bc78ccd15
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-01-20 15:11:08 +0100

    entering exercise

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let name = prompt("Qual è il tuo nome?");
+let surname = prompt("Qual è il tuo cognome?");
+let colour = prompt("Qual è il tuo colore preferito?");
+
+document.writeln("nomecognomecolorepreferito23");
